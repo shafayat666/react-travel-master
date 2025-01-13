7fca760f1ef859c8af7d901c002bb6e7be71f0af
shafayat666 <shafayatmahin5@gmail.com>
2025-01-13 17:13:38 +0600

    created basic layout and pages

---
+++ src/layouts/RootLayout.jsx
@@ -0,0 +1,11 @@
+import { Outlet } from "react-router-dom";
+
+const RootLayout = () => {
+  return (
+    <div>
+      <Outlet></Outlet>      
+    </div>
+  );
+};
+
+export default RootLayout;
--- src/main.jsx
+++ src/main.jsx
@@ -1,18 +1,9 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
+import { RouterProvider } from 'react-router-dom'
+import router from './routes/Routes'
 
-import {
-  createBrowserRouter,
-  RouterProvider,
-} from "react-router-dom";
-
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <div>Hello world!</div>,
-  },
-]);
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
